1be20a2b3d819367e66369cd3f529fadb298f035
helkem0 <helkem0@protonmail.com>
2024-09-02 03:38:48 +0800

    fixed config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["lightweight-charts", "react-loading-skeleton"],
+      external: ["lightweight-charts"],
     },
   },
   optimizeDeps: {
